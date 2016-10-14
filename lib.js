7134aa9e933d85aba75d8041c1e9c5f47c58921f
Chris Kuiper <chris.kuiper@gmail.com>
2016-10-14 07:51:21 -0500

    add function format as well as reduction procedures in succession

--- lib.js
+++ lib.js
@@ -20,6 +20,73 @@ const listedPrice =
         : 0
 
 /**
+ *  long hand for above
+*/
+const listedPrice2 =
+function (listing) {
+  return function (name) {
+    if (name === listing.name) {
+      return listing.price
+    } else {
+      return 0
+    }
+  }
+}
+
+/**
+ *  long hand for below
+*/
+const calculateTotals1 =
+listings =>
+carts => {
+  const result = []
+
+  for (let cart of carts) {
+    const customerTotal = { customer: cart.customer }
+    let total = 0
+
+    for (let item of cart.items) {
+      for (let listing of listings) {
+        if (listing.name === item) {
+          total += listing.price
+        }
+      }
+    }
+
+    customerTotal.total = total
+    result.push(customerTotal)
+  }
+  return result
+}
+
+/**
+ *  long hand for below using functions
+*/
+const calculatedTotals2 =
+function (listings) {
+  return function (carts) {
+    const result = []
+
+    for (let cart of carts) {
+      const customerTotal = { customer: cart.customer }
+      let total = 0
+
+      for (let item of cart.items) {
+        for (let listing of listings) {
+          if (listing.name === item) {
+            total += listing.price
+          }
+        }
+      }
+
+      customerTotal.total = total
+      result.push(customerTotal)
+    }
+    return result
+  }
+}
+
+/**
  * transform carts into an array of { customer, total }
  */
 const calculateTotals =
